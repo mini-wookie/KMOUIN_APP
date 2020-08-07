efc170427a530798acc839309aa12f8df8506d05
Croissanttttt <55791641+Croissanttttt@users.noreply.github.com>
2020-08-07 22:22:14 +0900

    Update BusPage.dart

--- lib/screens/BusPage.dart
+++ lib/screens/BusPage.dart
@@ -38,7 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        centerTitle: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Icon(CupertinoIcons.back),
@@ -59,119 +58,125 @@ class _MyHomePageState extends State<MyHomePage> {
           TopContainer(
             child: Image.asset('images/BusPage/TopContainer.png'),
           ),
-          SingleChildScrollView(
-            child: Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            SizedBox(height: 80.0),
-                            Text(
-                              "버스 정보",
-                              style: const TextStyle(
-                                color: const Color(0xffffffff),
-                                fontWeight: FontWeight.w500,
-                                fontFamily: "NotoSansKR",
-                                fontStyle: FontStyle.normal,
-                                fontSize: 32.0,
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                            SizedBox(height: 4.0),
-                            Text(
-                              "실시간 위치를 알 수 있어요",
-                              style: const TextStyle(
-                                color: const Color(0xffffffff),
-                                fontWeight: FontWeight.w300,
-                                fontFamily: "NotoSansKR",
-                                fontStyle: FontStyle.normal,
-                                fontSize: 20.0,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                            SizedBox(height: 43.0),
-                            BusCard(
-                                title: '셔틀 버스',
-                                width: 355 * fullWidth * rate,
-                                children: <Widget>[
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "해양대",
-                                  ),
-                                ]),
-                            SizedBox(height: 30.0),
-                            BusCard(
-                                title: '190번 버스',
-                                width: 355 * fullWidth * rate,
-                                children: <Widget>[
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "해양대",
-                                  ),
-                                  SizedBox(height: 14.0),
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "부산대",
-                                  ),
-                                  SizedBox(height: 14.0),
-                                  BusInfo(
-                                    width: 100 * fullWidth * rate,
-                                    title: "영도대교",
-                                  ),
-                                ]),
-                            SizedBox(height: 39.0),
-                            Container(
-                              width: 355 * fullWidth * rate,
-                              height: 100,
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
+          Column(
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Text(
+                "버스 정보",
+                style: const TextStyle(
+                  color: const Color(0xffffffff),
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "NotoSansKR",
+                  fontStyle: FontStyle.normal,
+                  fontSize: 32.0,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 4.0),
+              Text(
+                "실시간 위치를 알 수 있어요",
+                style: const TextStyle(
+                  color: const Color(0xffffffff),
+                  fontWeight: FontWeight.w300,
+                  fontFamily: "NotoSansKR",
+                  fontStyle: FontStyle.normal,
+                  fontSize: 20.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Flexible(
+                child: SingleChildScrollView(
+                  child: Container(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
-                                  Text(
-                                    "통근 버스 정보",
-                                    style: const TextStyle(
-                                      color: const Color(0xff131415),
-                                      fontWeight: FontWeight.w500,
-                                      fontFamily: "NotoSansKR",
-                                      fontStyle: FontStyle.normal,
-                                      fontSize: 28.0,
+                                  SizedBox(height: 43.0),
+                                  BusCard(
+                                      title: '셔틀 버스',
+                                      width: 355 * fullWidth * rate,
+                                      children: <Widget>[
+                                        BusInfo(
+                                          width: 100 * fullWidth * rate,
+                                          title: "해양대",
+                                        ),
+                                      ]),
+                                  SizedBox(height: 30.0),
+                                  BusCard(
+                                      title: '190번 버스',
+                                      width: 355 * fullWidth * rate,
+                                      children: <Widget>[
+                                        BusInfo(
+                                          width: 100 * fullWidth * rate,
+                                          title: "해양대",
+                                        ),
+                                        SizedBox(height: 14.0),
+                                        BusInfo(
+                                          width: 100 * fullWidth * rate,
+                                          title: "부산대",
+                                        ),
+                                        SizedBox(height: 14.0),
+                                        BusInfo(
+                                          width: 100 * fullWidth * rate,
+                                          title: "영도대교",
+                                        ),
+                                      ]),
+                                  SizedBox(height: 39.0),
+                                  Container(
+                                    width: 355 * fullWidth * rate,
+                                    height: 100,
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          "통근 버스 정보",
+                                          style: const TextStyle(
+                                            color: const Color(0xff131415),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily: "NotoSansKR",
+                                            fontStyle: FontStyle.normal,
+                                            fontSize: 28.0,
+                                          ),
+                                        ),
+                                        SizedBox(width: 22 * fullWidth * rate),
+                                        SizedBox(
+                                          width: 62,
+                                          height: 44,
+                                          child:
+                                          Image.asset("images/BusPage/bus_big_image.png"),
+                                        )
+                                      ],
+                                    ),
+                                    decoration: BoxDecoration(
+                                      borderRadius:
+                                      BorderRadius.all(Radius.circular(18)),
+                                      border: Border.all(
+                                          color: const Color(0xff842fb5), width: 1),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: const Color(0x80cacaca),
+                                            offset: Offset(0, -1),
+                                            blurRadius: 16,
+                                            spreadRadius: 2)
+                                      ],
+                                      color: const Color(0xffffffff),
                                     ),
                                   ),
-                                  SizedBox(width: 22 * fullWidth * rate),
-                                  SizedBox(
-                                    width: 62,
-                                    height: 44,
-                                    child:
-                                    Image.asset("images/BusPage/bus_big_image.png"),
-                                  )
                                 ],
                               ),
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(18)),
-                                border: Border.all(
-                                    color: const Color(0xff842fb5), width: 1),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: const Color(0x80cacaca),
-                                      offset: Offset(0, -1),
-                                      blurRadius: 16,
-                                      spreadRadius: 2)
-                                ],
-                                color: const Color(0xffffffff),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ]),
-            ),
+                            ],
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
